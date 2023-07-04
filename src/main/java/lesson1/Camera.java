87ecba4c56624497571d7f4ef7f83bc855f1e6cb
Andrii Lytvyn <litvin.a.a@gmail.com>
2023-07-04 21:47:39 +0200

    add spring

--- src/main/java/lesson1/Camera.java
+++ src/main/java/lesson1/Camera.java
@@ -7,6 +7,14 @@ public class Camera {
         this.cameraRoll = cameraRoll;
     }
 
+    public CameraRoll getCameraRoll() {
+        return cameraRoll;
+    }
+
+    public void setCameraRoll(CameraRoll cameraRoll) {
+        this.cameraRoll = cameraRoll;
+    }
+
     public void doPhotograph() {
         System.out.println("Click");
         cameraRoll.processing();
